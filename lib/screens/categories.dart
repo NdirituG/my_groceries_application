8d1e669818b4c163c8139b05ebcb03aabdf928b0
NdirituG <denisndiritug@gmail.com>
2022-10-14 16:01:48 +0300

    category_finishing_up

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:my_groceries_application/services/utils.dart';
 import 'package:my_groceries_application/widgets/categories_widget.dart';
+import 'package:my_groceries_application/widgets/text_widget.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: CategoriesWidget(),
-      ),
-    );
+    final utils = Utils(context);
+    Color color = utils.color;
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          title: TextWidget(
+            text: 'Categories',
+            color: color,
+            textSize: 24,
+            isTitle: true,
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.count(
+            crossAxisCount: 2,
+            childAspectRatio: 240 / 250,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            children: List.generate(6, (index) {
+              return CategoriesWidget();
+            }),
+          ),
+        ));
   }
 }
---
+++ lib/services/utils.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:my_groceries_application/provider/dark_theme_provider.dart';
+import 'package:provider/provider.dart';
+
+class Utils {
+  BuildContext context;
+  Utils(this.context);
+
+  bool get getTheme => Provider.of<DarkThemeProvider>(context).getDarkTheme;
+  Color get color => getTheme ? Colors.white : Colors.black;
+}
