40f4ed5a6c639edb530a86e9648180eec2fed0ce
NdirituG <denisndiritug@gmail.com>
2022-10-17 18:18:34 +0300

    handle UI if there are no products on sale

--- lib/inner_screens/on_sale_screen.dart
+++ lib/inner_screens/on_sale_screen.dart
@@ -11,6 +11,8 @@ class OnSaleScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //bool to check if there are items on sale
+    bool _isEmpty = false;
     final Color color = Utils(context).color;
     Size size = Utils(context).getScreenSize;
     return Scaffold(
@@ -34,14 +36,39 @@ class OnSaleScreen extends StatelessWidget {
           isTitle: true,
         ),
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: EdgeInsets.zero,
-        childAspectRatio: size.width / (size.height * 0.50),
-        children: List.generate(16, (index) {
-          return const OnSaleWidget();
-        }),
-      ),
+      body: _isEmpty
+          ? Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Image.asset(
+                        'assets/images/emptyOne.png',
+                      ),
+                    ),
+                    Text(
+                      'No products on sale yet!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: color,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          : GridView.count(
+              crossAxisCount: 2,
+              padding: EdgeInsets.zero,
+              childAspectRatio: size.width / (size.height * 0.50),
+              children: List.generate(16, (index) {
+                return const OnSaleWidget();
+              }),
+            ),
     );
   }
 }
